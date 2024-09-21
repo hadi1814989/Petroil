c5f18d27e192c11d66876105d7ba3c7d8def09ab
hadi1814989 <freelancer1814989@gmail.comgit config --global user.email freelancer1814989@gmail.com>
2024-09-21 22:26:15 +0600

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,10 +5,10 @@ const Contact = () => {
    <section className='bg-[#f40404] py-[46px]'>
     <div className="max-w-Container mx-auto">
     <div className="sm:flex">
-        <div className="sm:w-[80%]">
+        <div className="md:w-[80%] sm:w-[73%]">
             <h2 className='text-white font-pops font-bold text-[35px] text-center lg:text-[36px] sm:text-[26px] leading-[54px]'>Want to join as member branch in your area?</h2>
         </div>
-        <div className="sm:w-[20%] text-center">
+        <div className="md:w-[20%] sm:w-[25%] text-center">
             <button className='text-white py-[14px] sm:mt-0 mt-2 px-[31px] border-2 border-white bg-[#f40404] font-semibold font-pops text-[16px] hover:text-[#f40404] duration-200 text-right hover:bg-white'>CONTACT</button>
         </div>
     </div>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,9 +4,9 @@ import map from "../assets/map-img.png"
 const Location = () => {
   return (
     <section>
-        <div className=""> 
-           <img className='lg:h-[450px] h-[300px]' src={map} alt="" />
-        </div>
+           {/* <img className='lg:h-[450px] h-[300px]' src={map} alt="" /> */}
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.822575816588!2d90.42267047511406!3d23.718029178692525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9a8ca596f23%3A0xaf9c9c6504816d08!2sHumayun%20Shaheb%20Complex!5e0!3m2!1sen!2sbd!4v1726934020821!5m2!1sen!2sbd" width="100%" height="450" ></iframe>
+        
     </section>
   )
 }
